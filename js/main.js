3fec754c26b0a35a898ab0cad9cc3b388cc5bf2f
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-03-04 15:21:41 +0200

    add

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
+// start burger menu
 let headerMenu = document.querySelector('.header__menu')
 let burger = document.querySelector('.burger__container')
 let header = document.querySelector('.header')
-
-
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   headerMenu.classList.toggle('active')
@@ -17,8 +16,9 @@ for (let i of headerMenuItem) {
     header.classList.remove('active')
   })
 }
+// end burger menu
 
-// view more text
+// start view more text in main page
 if (document.querySelector('.three-block__text')) {
   let viewMoreBtn = document.querySelector('.three-block__view-more')
   let viewMoreText = document.querySelector('.three-block__text.hidden')
@@ -29,7 +29,7 @@ if (document.querySelector('.three-block__text')) {
 
   })
 }
-
+// end more text in main page
 
 // start custom slect 
 if (document.querySelector('.contact__form-select')) {
@@ -72,33 +72,31 @@ if (document.querySelector('.contact__form-select')) {
   })
 
 }
-
 // end custom slect 
 
-
 // start move tooltipe
-$('.tooltipLink').hover(function () {
-  var title = $(this).attr('data-tooltip');
-  $(this).data('tipText', title);
-  if (title == '') { }
-  else {
-    $('<p class="tooltip"></p>').fadeIn(200).text(title).appendTo('body');
-  }
-}, function () {
-  $(this).attr('data-tooltip', $(this).data('tipText'));
-  $('.tooltip').fadeOut(200);
-}).mousemove(function (e) {
-  var mousex = e.pageX;
-  var mousey = e.pageY;
-  $('.tooltip').css({
-    top: mousey,
-    left: mousex,
-    dispaly: 'flex'
-  })
-});
+if (document.querySelector('.tooltipLink')) {
+  $('.tooltipLink').hover(function () {
+    var title = $(this).attr('data-tooltip');
+    $(this).data('tipText', title);
+    if (title == '') { }
+    else {
+      $('<p class="tooltip"></p>').fadeIn(200).text(title).appendTo('body');
+    }
+  }, function () {
+    $(this).attr('data-tooltip', $(this).data('tipText'));
+    $('.tooltip').fadeOut(200);
+  }).mousemove(function (e) {
+    var mousex = e.pageX;
+    var mousey = e.pageY;
+    $('.tooltip').css({
+      top: mousey,
+      left: mousex,
+      dispaly: 'flex'
+    })
+  });
+}
 // end move tooltipe
-
-
 
 // start more text about us
 if (document.querySelector('.two-block__about-more')) {
@@ -112,3 +110,54 @@ if (document.querySelector('.two-block__about-more')) {
   })
 }
 // end more text about us
+
+// start config range
+if (document.querySelector('.contact__range')) {
+  let rangePriceOutput = document.querySelector('.contact__range-price-js')
+  let range = document.querySelector('.contact__range')
+  let rangeMin = document.querySelector('.contact_range-min')
+  let rangeMax = document.querySelector('.contact_range-max')
+  let rangeValue = document.querySelector('.contact_range-value')
+  range.min = rangeMin.textContent
+  range.max = rangeMax.textContent
+  range.value = rangeValue.textContent
+  rangePriceOutput.textContent = range.value
+  // for desktop
+  document.addEventListener('mousemove', () => {
+    rangePriceOutput.textContent = range.value
+  })
+  //for mobiel
+  document.addEventListener('change', () => {
+    rangePriceOutput.textContent = range.value
+  })
+}
+// end config range
+
+// start view more items in about page
+if (document.querySelector('.about-scroll__skill-block')) {
+  const skillBlock = document.querySelectorAll('.about-scroll__skill-block')
+  for (let containerItem of skillBlock) {
+    let skillItem = containerItem.querySelectorAll('.about-scroll__skill')
+    for (let item of skillItem) {
+      item.classList.add('hidden')
+    }
+    for (let i = 0; i < 4; i++) {
+      skillItem[i].classList.remove('hidden')
+    }
+
+    // view more items on click
+    let btnViewMore = containerItem.querySelector('.about-scroll__skill-more')
+    btnViewMore.addEventListener('click', () => {
+      btnViewMore.classList.add('hidden')
+      let skillItem = containerItem.querySelectorAll('.about-scroll__skill')
+      for (let item of skillItem) {
+        if (item.classList.remove('hidden')) {
+          item.classList.contains('hidden')
+        }
+      }
+    })
+
+  }
+
+}
+// end view more items in about page
